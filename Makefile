d519be28d973077107908cc83504f7e2fc3a5fba
杨淏楠 <163834262+yanghn2002@users.noreply.github.com>
2025-03-13 09:30:57 +0800

    update example

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
-include hikrobot_opencv.mk
-
 CXX = g++
+
+include hikrobot_opencv.mk
 
 build_example: hikrobot_opencv_example.cpp
 	mkdir -p build
-	$(CXX) hikrobot_opencv_example.cpp -o build/hikrobot_opencv_example $(HIKCV_CV_FLAGS) -lopencv_highgui $(HIKCV_MV_FLAGS) -O3
+	$(CXX) hikrobot_opencv_example.cpp -o build/hikrobot_opencv_example $(HIKCV_CV_FLAGS) $(HIKCV_MV_FLAGS) -O3 \
+	-lopencv_imgproc -lopencv_highgui
--- hikrobot_opencv_example.cpp
+++ hikrobot_opencv_example.cpp
@@ -2,29 +2,40 @@
 #include <iostream>
 
 
+namespace hik = hikcv;
+
+
 int main(void) {
 
     // get HikrobotMvContext instance
-    hikcv::MvContext& context = hikcv::MvContext::instance();
+    hik::MvContext& context = hik::MvContext::instance();
 
     // enum Hikrobot Mv devices
-    hikcv::MvContext::DeviceIndex devices = context.enumDevices();
+    hik::MvContext::DeviceIndex devices = context.enumDevices();
     std::cout << "devices:" << std::endl;
     for(auto device: devices)
         std::cout << '\t' << device.first << ": " << device.second << std::endl;
     
     // open camera and start grabbing
     std::string input_serial_number;
-    std::cout << "input camera serial number to open: " << std::flush;
+    std::cout << "input camera index or serial number to open: " << std::flush;
     std::getline(std::cin, input_serial_number);
-    hikcv::CvCapture camera = context.createCapture(input_serial_number);
+    hik::CvCapture camera = context.createCapture(input_serial_number);
     camera.start();
 
     // get OpenCV Mat and show
     while(true) {
         cv::Mat image = camera.ref_cv_mat();
-        cv::imshow("image cv", image);
-        cv::waitKey(1);
+        auto width = image.cols;
+        auto height = image.rows;
+        while(width >= 1000 || height >= 1000) {
+            width /= 2;
+            height /= 2;
+        }
+        cv::Mat image_show;
+        cv::resize(image, image_show, cv::Size(width, height));
+        cv::imshow("Hikrobot OpenCV Cpp example (ESC to exit)", image_show);
+        if(27 == cv::waitKey(1)) break;
     }
 
     return 0;
